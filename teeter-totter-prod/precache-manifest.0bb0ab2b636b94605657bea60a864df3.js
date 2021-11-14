0cf52c3443c56088379e14a3bf839c2fd832bd1c
Cacciat0r3 <cassiat0r3@gmail.com>
2021-11-14 14:49:15 +0200

    Add files via upload

---
+++ teeter-totter-prod/precache-manifest.0bb0ab2b636b94605657bea60a864df3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d14acabda2b0f8999bb",
+    "url": "/teeter-totter-prod/css/app.c199dcb9.css"
+  },
+  {
+    "revision": "8346c4dfcaaf4062e471",
+    "url": "/teeter-totter-prod/css/chunk-0bcff806.ed46108f.css"
+  },
+  {
+    "revision": "e308a3e575e6de0f26b3",
+    "url": "/teeter-totter-prod/css/chunk-13a0654a.59481415.css"
+  },
+  {
+    "revision": "b73faaf9248092f74b63",
+    "url": "/teeter-totter-prod/css/chunk-fbb2c164.3a65b300.css"
+  },
+  {
+    "revision": "95fd9c8977fd23d3c663052acfa90545",
+    "url": "/teeter-totter-prod/index.html"
+  },
+  {
+    "revision": "2d14acabda2b0f8999bb",
+    "url": "/teeter-totter-prod/js/app.4a9dcb46.js"
+  },
+  {
+    "revision": "8346c4dfcaaf4062e471",
+    "url": "/teeter-totter-prod/js/chunk-0bcff806.18e31bad.js"
+  },
+  {
+    "revision": "e308a3e575e6de0f26b3",
+    "url": "/teeter-totter-prod/js/chunk-13a0654a.d7dde1a4.js"
+  },
+  {
+    "revision": "b73faaf9248092f74b63",
+    "url": "/teeter-totter-prod/js/chunk-fbb2c164.2d40b44e.js"
+  },
+  {
+    "revision": "7ff9335b864d8e0ea719",
+    "url": "/teeter-totter-prod/js/chunk-vendors.2649cfbb.js"
+  },
+  {
+    "revision": "1854e5c9c7349979dbeb4f11e557bb24",
+    "url": "/teeter-totter-prod/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/teeter-totter-prod/robots.txt"
+  }
+]);
